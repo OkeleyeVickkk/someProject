3960387dd3e9477695a4fa03d96d11e06adabb60
OkeleyeVickkk <justusofnigeria@gmail.com>
2024-10-29 05:53:48 +0100

    making changes

---
+++ assets/js/index.custom.js
@@ -0,0 +1,19 @@
+// const elem = document.querySelector(".v-mouse-ele");
+
+// document.addEventListener("mousemove", function (e) {
+// 	const { clientX, clientY } = e;
+// 	elem.classList.add("active");
+// 	elem.style.setProperty("--x", `${clientX}px`);
+// 	elem.style.setProperty("--y", `${clientY}px`);
+// });
+
+// document.addEventListener("mouseout", () => elem.classList.remove("active"));
+
+// for (const each of document.querySelectorAll(".v-289fh29b")) {
+// 	each.addEventListener("mouseenter", (e) => {
+// 		e.target.classList.add("active");
+// 	});
+// 	each.addEventListener("mouseleave", (e) => {
+// 		e.target.classList.remove("active");
+// 	});
+// }
