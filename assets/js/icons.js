1c7a06d7f66697f1ded48cce0b02b3b02a52f4d6
OkeleyeVickkk <justusofnigeria@gmail.com>
2024-10-29 15:13:14 +0100

    updates made

---
+++ assets/js/icons.js
@@ -0,0 +1,18 @@
+export const X = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <rect width="24" height="24" fill="none" />
+    <path fill="currentColor" d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12L5.7 16.89a.996.996 0 1 0 1.41 1.41L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4" />
+  </svg>
+`;
+export const V = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+  <rect width="16" height="16" fill="none" />
+	<path
+	  fill="none"
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="1.5"
+		d="M3.75 5.75L8 10.25l4.25-4.5" />
+	</svg>
+`;
--- assets/js/index.custom.js
+++ assets/js/index.custom.js
@@ -1,3 +1,5 @@
+import { X, V } from "./icons.js";
+
 // const elem = document.querySelector(".v-mouse-ele");
 
 // document.addEventListener("mousemove", function (e) {
@@ -17,3 +19,28 @@
 // 		e.target.classList.remove("active");
 // 	});
 // }
+
+(() => {
+	const initSubHeader = function () {
+		const pageHeaderSubHeader = document.querySelector("#v-page-header .v-subheader");
+		if (!pageHeaderSubHeader) return;
+		const toggler = pageHeaderSubHeader.querySelector(".v-toggle-lg-screen-auth-dropdown .v-toggler");
+		if (!toggler) return;
+		toggler.addEventListener("click", function () {
+			const target = this;
+			const iconHolder = target.querySelector(".v-icon");
+			if (pageHeaderSubHeader.classList.contains("active")) {
+				pageHeaderSubHeader.classList.remove("active");
+				iconHolder.innerHTML = V;
+			} else {
+				pageHeaderSubHeader.classList.add("active");
+				iconHolder.innerHTML = X;
+			}
+		});
+	};
+
+	function init() {
+		initSubHeader();
+	}
+	window.addEventListener("DOMContentLoaded", init);
+})();
