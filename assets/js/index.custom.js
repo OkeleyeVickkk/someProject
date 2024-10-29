8553bfa8662ab016380d3d5804aa3f0f6edc082e
OkeleyeVickkk <justusofnigeria@gmail.com>
2024-10-29 17:18:43 +0100

    updates

--- assets/js/index.custom.js
+++ assets/js/index.custom.js
@@ -1,25 +1,5 @@
 import { X, V, countries, EYESLASH, EYE } from "./helpers.js";
 
-// const elem = document.querySelector(".v-mouse-ele");
-
-// document.addEventListener("mousemove", function (e) {
-// 	const { clientX, clientY } = e;
-// 	elem.classList.add("active");
-// 	elem.style.setProperty("--x", `${clientX}px`);
-// 	elem.style.setProperty("--y", `${clientY}px`);
-// });
-
-// document.addEventListener("mouseout", () => elem.classList.remove("active"));
-
-// for (const each of document.querySelectorAll(".v-289fh29b")) {
-// 	each.addEventListener("mouseenter", (e) => {
-// 		e.target.classList.add("active");
-// 	});
-// 	each.addEventListener("mouseleave", (e) => {
-// 		e.target.classList.remove("active");
-// 	});
-// }
-
 (() => {
 	const initSubHeader = function () {
 		const pageHeaderSubHeader = document.querySelector("#v-page-header .v-subheader");
@@ -49,7 +29,7 @@ import { X, V, countries, EYESLASH, EYE } from "./helpers.js";
 			const firstChild = `<option value="" disabled selected>Select country</option>`;
 			countrySelectWrapper.innerHTML = firstChild;
 			for (const country of countries) {
-				const newOption = `<option value="${country.nationality}">${country.nationality}</option>`;
+				const newOption = `<option value="${country.name}">${country.name}</option>`;
 				countrySelectWrapper.innerHTML += newOption;
 			}
 		});
